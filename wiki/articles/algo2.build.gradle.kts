8095115d82a2345f7d7ff104d5848aac78240956
Guillermo Bianchi <gbianchi@estudiantes.unsam.edu.ar>
2023-04-20 19:05:17 -0300

    Activar generación de csv en jacoco, para el ejemplo de Action ofrecido.

--- wiki/articles/algo2.build.gradle.kts
+++ wiki/articles/algo2.build.gradle.kts
@@ -46,7 +46,7 @@ jacoco {
 tasks.jacocoTestReport {
     reports {
         xml.required.set(true)
-        csv.required.set(false)
+        csv.required.set(true)
         html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
     }
 }
